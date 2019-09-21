9f110b4f28f4a2b8362e6e83ae5710bbc2d87b18
Enyel <enyelsequeira@hotmail.com>
2019-09-21 16:22:46 +0800

    CORS IMPLEMENTED
    cors was implementd

--- index.js
+++ index.js
@@ -2,177 +2,153 @@
 const express = require ('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
-const uuid = require("uuid");
-const mongoose = require ('mongoose');
-const cors = require('cors');
-const validator = require('express-validator');
-const passport = require ('passport');
-const Models = require('./models.js');
-
+const uuid = require('uuid');
+// passport
+const passport = require('passport');
 require('./passport');
-//crating var to use express functionality
-const app = express();
+//Importing mongoose
+const mongoose = require ('mongoose');
+const Models = require('./models.js');
 
 const Movies = Models.Movie;
 const Users = Models.User;
 
-//connect mongoose to database
-mongoose.connect('mongodb://localhost:27017/myflix2', {useNewUrlParser: true});
+//crating var to use express functionality
+const app = express();
+// servers documentation.html file from public folder
+app.use(express.static('public'));
+
+app.use(bodyParser.json());
 
 //log request using morgans's 
 app.use(morgan('common'));
-// servers documentation.html file from public folder
-app.use(express.static('public'));
-//body parser
-app.use(bodyParser.json());
-//using cors
-app.use(cors());
 
-//import aurthentication
+//connect mongoose to database
+mongoose.connect('mongodb://localhost:27017/myflix2', {useNewUrlParser: true});
+
+//importing auth.js file
 var auth = require('./auth')(app);
 
-//express validator
-app.use(validator());
+
+
+
+
 
 /*//list of movies
 let movies = [ {
-    Title: 'avatar',
-    Description: 'a paralyzed former Marine, becomes mobile again through one such Avatar and falls in love with a Navi woman',
-    Genre:{
-    Name:"Fanstasy",
-    Description:"Fantasy is a genre of speculative fiction set in a fictional universe."
-    },
-    Director: {
+    title: 'avatar',
+    description: 'a paralyzed former Marine, becomes mobile again through one such Avatar and falls in love with a Navi woman',
+    genre: 'Fantasy ',
+    director: {
         name: 'James Cameron',
         bio: 'is a Canadian filmmaker and environmentalist who currently lives in New Zealand and the United States',
         dateOfBirth: '1956',
         deathYear:'n/a',
     },
-    Image: 'https://www.imdb.com/title/tt0499549/mediaviewer/rm843615744',
-    Featured:true
+    image: 'https://www.imdb.com/title/tt0499549/mediaviewer/rm843615744',
 }, 
- { 
-    Title: 'Iron man',
-    Description: 'Returning to America, Stark refines the suit and uses it to combat crime and terrorism',
-    Genre:{
-    Name:"Thriller",
-    Description:"Thriller film, also known as suspense film or suspense thriller, is a broad film genre that involves excitement and suspense in the audience."
-    },
-    Director: {
+{ 
+    title: 'Iron man',
+    description: 'Returning to America, Stark refines the suit and uses it to combat crime and terrorism',
+    genre: 'Thriller',
+    director: {
         name: 'John Favreau',
         bio: 'is an American actor, director, producer, and screenwriter. He is known for his work with the Marvel Cinematic Universe',
         dateOfBirth: '1966',
         deathYear:'n/a',
     },
-    Image: 'https://www.imdb.com/title/tt0371746/mediaviewer/rm1544850432',
-    Featured:true
+    image: 'https://www.imdb.com/title/tt0371746/mediaviewer/rm1544850432',
 },
 {
-    Title: 'Men in black',
-    Description: 'They are the best-kept secret in the universe. Working for a highly funded yet unofficial government agency',
-    Genre:{
-    Name:"Thriller",
-    Description:"Thriller film, also known as suspense film or suspense thriller, is a broad film genre that involves excitement and suspense in the audience."
-    },
-    Director: {
+    title: 'Men in black',
+    description: 'They are the best-kept secret in the universe. Working for a highly funded yet unofficial government agency',
+    genre: 'Thriller',
+    director: {
         name: 'Barry Sonnenfeld',
         bio: 'Barry Sonnenfeld is an American filmmaker and television director. He originally worked as a cinematographer for the Coen brothers before directing films such as The Addams Family and its sequel Addams Family Values',
         dateOfBirth: '1953',
         deathYear:'n/a',
     },
-    Image: 'https://www.imdb.com/title/tt0119654/mediaviewer/rm2364027904',
-    Featured:true
+    image: 'https://www.imdb.com/title/tt0119654/mediaviewer/rm2364027904',
 
 },
 {
-    Title: 'the lion king',
-    Description: 'Simba idolizes his father, King Mufasa, and takes to heart his own royal destiny on the plains of Africa.',
-    Genre:{
-    Name:"Drama",
-    Description:"Drama is the specific mode of fiction represented in performance: a play, opera, mime, ballet, etc."
-    },
-    Director: {
+    title: 'the lion king',
+    description: 'Simba idolizes his father, King Mufasa, and takes to heart his own royal destiny on the plains of Africa.',
+    genre: 'Drama',
+    director: {
         name: 'John Favreau',
         bio: 'is an American actor, director, producer, and screenwriter. He is known for his work with the Marvel Cinematic Universe',
         dateOfBirth: '1966',
         deathYear:'n/a',
     },
-    Image: 'https://www.imdb.com/title/tt6105098/mediaviewer/rm2458872832',
-    Featured:true
+    image: 'https://www.imdb.com/title/tt6105098/mediaviewer/rm2458872832',
 }, 
    
 {
-    Title: 'star wars return of the jedi',
-    Description: 'Luke Skywalker (Mark Hamill) battles horrible Jabba the Hut and cruel Darth Vader to save his comrades in the Rebel Alliance and triumph over the Galactic Empire',
-    Genre:{
-    Name:"Fanstasy",
-    Description:"Fantasy is a genre of speculative fiction set in a fictional universe."
-    },
-    Director: {
+    title: 'star wars return of the jedi',
+    description: 'Luke Skywalker (Mark Hamill) battles horrible Jabba the Hut and cruel Darth Vader to save his comrades in the Rebel Alliance and triumph over the Galactic Empire',
+    genre: 'Fantasy',
+    director: {
         name: 'George Lucas',
         bio: 'George Walton Lucas Jr. is an American filmmaker and entrepreneur. Lucas is known for creating the Star Wars and Indiana Jones franchises and founding Lucasfilm, LucasArts and Industrial Light & Magic',
         dateOfBirth: '1944',
         deathYear:'n/a',
     },
-    Image: 'https://www.imdb.com/title/tt0086190/mediaviewer/rm602420224',
-    Featured:true
+    image: 'https://www.imdb.com/title/tt0086190/mediaviewer/rm602420224',
 },
 {
-    Title: 'Avengers',
-    Description: 'Nick Fury (Samuel L. Jackson), director of S.H.I.E.L.D., initiates a superhero recruitment effort to defeat the unprecedented threat to Earth',
-    Genre:{
-    Name:"Fanstasy",
-    Description:"Fantasy is a genre of speculative fiction set in a fictional universe."
-    },
-    Director: {
+    title: 'Avengers',
+    description: 'Nick Fury (Samuel L. Jackson), director of S.H.I.E.L.D., initiates a superhero recruitment effort to defeat the unprecedented threat to Earth',
+    genre: 'Sci-fi ',
+    director: {
         name: 'John Favreau',
         bio: 'is an American actor, director, producer, and screenwriter. He is known for his work with the Marvel Cinematic Universe',
         dateOfBirth: '1966',
         deathYear:'n/a',
     },
-    Image: 'https://www.imdb.com/title/tt0848228/mediaviewer/rm3955117056',
-    Featured:true
+    image: 'https://www.imdb.com/title/tt0848228/mediaviewer/rm3955117056',
 },
 {
-    Title: 'x-men',
-    Description: 'They are children of the atom, homo superior, the next link in the chain of evolution.',
-    Genre:{
-    Name:"Sci-fi",
-    Description:"It typically deals with imaginative and futuristic concepts such as advanced science and technology, time travel, parallel universes, fictional worlds, space exploration"
-    },
-    Director: {
+    title: 'x-men',
+    description: 'They are children of the atom, homo superior, the next link in the chain of evolution.',
+    genre: 'Sci-fi ',
+    director: {
         name: 'Bryan Singer',
         bio: 'Bryan Jay Singer is an American director, producer and writer of film and television. He is the founder of Bad Hat Harry Productions and has produced or co-produced almost all of the films he has directed.',
         dateOfBirth: '1944',
         deathYear:'n/a',
     },
-    Image: 'https://www.imdb.com/title/tt0120903/mediaviewer/rm1905724928',
-    Featured:true
+    image: 'https://www.imdb.com/title/tt0120903/mediaviewer/rm1905724928',
 },
-
 { 
-    Title:"Silence of the Lambs",
-    Description:"A young FBI cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer.",
-    Genre:{
-    Name:"Thriller",
-    Description:"Thriller film, also known as suspense film or suspense thriller, is a broad film genre that involves excitement and suspense in the audience."
+    title: 'Silece of the Lambs',
+    description: 'A young FBI cadet must receive the help of an incarcerated and manipulative cannibal killer to help catch another serial killer.',
+    genre: 'Thriller',
+    director: {
+        name: 'John Favreau',
+        bio: 'Robert Jonathan Demme was an American director, producer, and screenwriter.',
+        dateOfBirth: '1944',
+        deathYear:'2017',
     },
-    Director:{
-    Name:"Jonathan Demme",
-    Bio:"Robert Jonathan Demme was an American director, producer, and screenwriter.",
-    Birth:"1944",
-    Death:"2017"
+    image: 'https://www.imdb.com/title/tt0102926/mediaviewer/rm3242988544',
+},
+{ 
+    title: 'The Dark Knight Rises',
+    description: 'It has been eight years since Batman (Christian Bale), in collusion with Commissioner Gordon (Gary Oldman), vanished into the night. Assuming responsibility for the death of Harvey Dent, Batman sacrificed everything for what he and Gordon hoped would be the greater good',
+    genre: 'Thriller',
+    director: {
+        name: 'Christoper Nolan',
+        bio: 'Christopher Edward Nolan, CBE is an English-American film director, screenwriter, and producer, who is known for making personal, distinctive films within the Hollywood mainstream.',
+        dateOfBirth: '1970',
+        deathYear:'n/a',
     },
-    ImagePath:"silenceofthelambs.png",
-    Featured:true
+    image: 'https://www.imdb.com/title/tt1345836/mediaviewer/rm834516224',
 },
 {
     title : 'Hercules',
     description: 'Though he is famous across the ancient world for his larger-than-life exploits,',
-    Genre:{
-    Name:"Fanstasy",
-    Description:"Fantasy is a genre of speculative fiction set in a fictional universe."
-    },
+    genre: 'Fantasy',
     director: {
         name: 'Brett Ratner',
         bio: 'Brett Ratner is an American director and producer. He directed the Rush Hour film series, The Family Man, Red Dragon, X-Men: The Last Stand, and Tower Heist.',
@@ -180,7 +156,6 @@ let movies = [ {
         deathYear:'n/a',
     },
     image: 'https://www.imdb.com/title/tt0119282/mediaviewer/rm944837888',
-    Featured:true
 }];
 
 let users =[{
@@ -214,6 +189,9 @@ let users =[{
     dateOfBirth: ('1995-02-19'),
     FavoriteMovies: ["5d70a859a1de173d18b1726c", "5d70a859a1de173d18b1726c", "5d70a5daa1de173d18b17269", "5d709d9e68e54b94f24"]
 }];*/
+
+
+
 
 //gets a list of  all movies
 app.get('/movies', passport.authenticate('jwt',{ session: false
@@ -288,45 +266,32 @@ app.get('/movies/director/:Name', passport.authenticate('jwt',{ session: false})
   });
 
 
+
 //allow new user to register 
-app.post('/users', function(req, res)  {
-    req.checkBody('Username', 'Username is required').notEmpty();
-    req.checkBody('Username', 'Username contains non alphanumeric characters - not allowed.').isAlphanumeric()
-    req.checkBody('Password', 'Password is required').notEmpty();
-    req.checkBody('Email', 'Email is required').notEmpty();
-    req.checkBody('Email', 'Email does not appear to be valid').isEmail();
-    // check the validation object for errors
-      var errors = req.validationErrors();
-      if (errors) {
-        return res.status(422).json({ errors: errors });
-      }
-    var hashedPassword = Users.hashPassword(req.body.Password);
-    Users.findOne({ Username : req.body.Username }) // Search to see if a user with the requested username already exists
-    .then(function(user) {
-      if (user) {
-        //If the user is found, send a response that it already exists
-        return res.status(400).send(req.body.Username + " already exists");
-      } else {
-        Users
-        .create({
-          Username : req.body.Username,
-          Password: hashedPassword,
-          Email : req.body.Email,
-          Birthday : req.body.Birthday
-        })
-        .then(function(user) { res.status(201).json(user) })
-        .catch(function(error) {
-          console.error(error);
-          res.status(500).send("Error: " + error);
-        });
-      }
-    }).catch(function(error) {
+app.post('/users', function(req, res){
+Users.findOne({Username: req.body.Username })
+  .then(function(user){
+    if(user){
+      return res.status(400).send(req.body.Username + " already exists.");
+    }else{
+      Users
+      .create ({
+        Username: req.body.Username,
+        Password: req.body.Password,
+        Email: req.body.Email,
+        Birthday: req.body.Birthday
+      })
+      .then(function(user) {res.status(201).json(user) })
+      .catch(function(error){
+        console.error(error);
+        res.status(500).send("Error: " + error);
+      })
+    }
+  }).catch(function(error){
       console.error(error);
       res.status(500).send("Error: " + error);
-    });
   });
-  
-
+});
 /*
 app.post('/users', function(req, res){
     let newUser = req.body;
@@ -418,7 +383,7 @@ app.post('/users/:Username/Favorite/:MovieID', passport.authenticate('jwt',{ ses
 
 //delete from favorites //cant get postman
 
-app.delete('/users/:Username/Favorite /:MovieID', function(req, res){
+app.delete('/users/:Username/Favorite/:MovieID', function(req, res){
     Users.findOneAndUpdate({Username: req.params.Username},{
         $pull : {Favorites : req.params.MovieID}
     },
@@ -474,15 +439,12 @@ app.get('/', function(req, res){
     res.send('Welcome to my favorite movies');
 });
 
-
 app.use(function(err, req, res, next){
     console.error(err.stack);
     res.status(500).send('something is wrong');
 });
-
 //listening for request
-var port = process.env.PORT || 3000;
-app.listen(port, "0.0.0.0", function() {
-console.log("Listening on Port 3000");
-});
+app.listen(8080, () => 
+console.log('this is listening to you on port 8080')
+);
 
