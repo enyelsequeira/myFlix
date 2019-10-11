5f02bccb57d72527efd65b9d0c926afd251ec91a
Enyel <enyelsequeira@hotmail.com>
2019-10-11 22:45:40 +0800

    Client
    Client side changes to movie view/ card/index

--- client/src/components/main-view.jsx
+++ client/src/components/main-view.jsx
@@ -1,41 +1,72 @@
-import React from 'react';
-import axios from 'axios';
+import React from "react";
+import axios from "axios";
 
-class MainView extends React.Component {
-    //of the hooks available in a react component
-    componentDidMount(){
-        axios.get('<my-api-endpoint/movie>')
-        .then(response => {
-            //assing result to the state
-            this.setState({
-                movies: response.data
-            });
-        })
-        .catch (function (error){
-            console.log(error);            
+import { MovieCard } from "./movie-card.jsx";
+import { MovieView } from "./movie-view.jsx";
+
+export class MainView extends React.Component {
+  //of the hooks available in a react component
+
+  constructor() {
+    //constructor so react can initialize
+
+    super();
+    //initialize the state to an empty objec so we can destructure it
+    this.state = {
+      movies: [],
+      selectedMovie: null
+    };
+  }
+  componentDidMount() {
+    axios
+      .get("https://sheltered-scrubland-70732.herokuapp.com/movies")
+      .then(res => {
+        //assing result to the state
+        this.setState({
+          movies: res.data
         });
-    }
-    constructor (){
-        //constructor so react can initialize
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
+  onMovieClick(movie) {
+    this.setState({
+      selectedMovie: movie
+    });
+  }
 
-        super();
-        //initialize the state to an empty objec so we can destructure it 
-        this.state={};
-    }
+  //button
+  onButtonClick() {
+    this.setState({
+      selectedMovie: null
+    });
+  }
 
-    //this overrides the render() method of the superclass
-    render(){
-        const {movies}= this.state;
+  //this overrides the render() method of the superclass
+  render() {
+    const { movies, selectedMovie } = this.state;
 
-        //before the movies have been loaded
-        if(!movies)return <div className= "main-view"/>;
+    //before the movies have been loaded
+    if (!movies) return <div className="main-view" />;
 
-        return (
-            <div className= "main-view"></div>
-            { movies.map(movie => (
-                <div className= "movie-card" key = {movie._id}> {movie.Title}</div>
-            ))}
-            </div>
-        );
-    }
+    return (
+      <div className="main-view">
+        {selectedMovie ? (
+          <MovieView
+            movie={selectedMovie}
+            onClick={movie => this.onButtonClick()}
+          />
+        ) : (
+          movies.map(movie => (
+            <MovieCard
+              key={movie._id}
+              movie={movie}
+              onClick={movie => this.onMovieClick(movie)}
+            />
+          ))
+        )}
+      </div>
+    );
+  }
 }
